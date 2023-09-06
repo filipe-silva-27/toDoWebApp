d25e6ca63c4dc66dae7278d4267a6e39528d18ad
filipe-silva-27 <filipesprogramming@gmail.com>
2023-09-06 09:48:49 +0100

    Adding git to project

---
+++ src/main/java/com/in28minutes/springboot/firstspringwebapp/FirstspringwebappApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.firstspringwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/springboot/firstspringwebapp/FirstspringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.firstspringwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
