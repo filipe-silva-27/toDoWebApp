bd30e13963f77a922138c27ec052dc62c3efe176
filipe-silva-27 <filipesprogramming@gmail.com>
2023-09-06 18:48:50 +0100

    feat: login form added

--- src/main/java/com/in28minutes/springboot/firstspringwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/firstspringwebapp/login/LoginController.java
@@ -1,26 +1,17 @@
 package com.in28minutes.springboot.firstspringwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class LoginController {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	///login => com.in28minutes.springboot.myfirstwebapp.login.LoginController => login.jsp
 
 	//Model is used to pass anything to a jsp page
-
 	@RequestMapping("login")
-	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
-		logger.debug("Name is : {}", name); // NOT RECOMMENDED FOR PRODUCTION
+	public String gotoLoginPage() {
 		return "login";
 	}
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -5,5 +5,15 @@
     <body>
         <%--expression language is used to use variables passed--%>
         Welcome to the login page ${name}!
+
+        <form method="post">
+            Name: <label>
+            <input type="text" name="name"/>
+        </label><br/>
+            Password: <label>
+            <input type="password" name="password"/>
+        </label><br/>
+            <input type="submit" value="Login"/>
+        </form>
     </body>
 </html>
