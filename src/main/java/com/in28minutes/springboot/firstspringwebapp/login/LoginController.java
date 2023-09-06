ec19c027de5690ee1621280e863865c28b83204f
filipe-silva-27 <filipesprogramming@gmail.com>
2023-09-06 15:32:23 +0100

    fix: added logger to login controller

--- src/main/java/com/in28minutes/springboot/firstspringwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/firstspringwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.firstspringwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	///login => com.in28minutes.springboot.myfirstwebapp.login.LoginController => login.jsp
 
 	//Model is used to pass anything to a jsp page
@@ -16,7 +20,7 @@ public class LoginController {
 	@RequestMapping("login")
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
-		System.out.println("Name is : " + name); // NOT RECOMMENDED FOR PRODUCTION
+		logger.debug("Name is : {}", name); // NOT RECOMMENDED FOR PRODUCTION
 		return "login";
 	}
 }
