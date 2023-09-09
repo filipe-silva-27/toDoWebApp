f9dada99a76e22d2705af937a48e7a9efd27d645
filipe-silva-27 <filipesprogramming@gmail.com>
2023-09-09 09:55:36 +0100

    refactor: simplifying annotations

--- src/main/java/com/in28minutes/springboot/firstspringwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/firstspringwebapp/login/LoginController.java
@@ -1,16 +1,13 @@
 package com.in28minutes.springboot.firstspringwebapp.login;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginController {
 
-	private AutheticationService service;
+	private final AutheticationService service;
 
 	//constructor injection
 	 public LoginController(AutheticationService service) {
@@ -20,12 +17,14 @@ public class LoginController {
 	//only when the request is a GET request, this method
 	// will be called because for POST  (fll the form)
 	// it will redirect to welcome
-	@RequestMapping(value="login", method = RequestMethod.GET)
+	@GetMapping(value="login")
+	//same as 	@RequestMapping(value="login", method = RequestMethod.GET)
 	public String gotoLoginPage() {
 		return "login";
 	}
 
-	@RequestMapping(value="login", method = RequestMethod.POST)
+	@PostMapping (value="login")
+	//same as @RequestMapping(value="login", method = RequestMethod.POST)
 	public String gotoWelcomePage(
 		@RequestParam String name, @RequestParam String password, ModelMap model) {
 
